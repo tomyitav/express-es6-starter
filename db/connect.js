9ada4558f36731641e9308a1ab72b8294b992dd1
Tom <tomyitav@gmail.com>
2017-09-10 22:38:47 +0300

    Async connect

--- db/connect.js
+++ db/connect.js
@@ -2,18 +2,17 @@ import Mongoose from 'mongoose';
 import logger from '../core/logger/app-logger'
 import config from '../core/config/config.dev'
 
-const connectToDb = () => {
+const connectToDb = async () => {
     let dbHost = config.dbHost;
     let dbPort = config.dbPort;
     let dbName = config.dbName;
-    Mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, (err) => {
-        if (err) {
-            logger.error('Could not connect to MongoDB');
-        }
-        else {
-            logger.info('Connected to mongo!!!');
-        }
-    });
+    try {
+        await Mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`);
+        logger.info('Connected to mongo!!!');
+    }
+    catch (err) {
+        logger.error('Could not connect to MongoDB');
+    }
 }
 
 export default connectToDb;
